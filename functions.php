396c9e7e4b27aaab0453193cfe2a93019b53e137
kovarnyirus <kovarnyi@gmail.com>
2018-02-08 17:56:52 +0300

    e2

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
-function template_page ($address_template, $array_data) {
+function render_template ($address_template, $array_data) {
+    if (!file_exists($address_template)){
+      return  print ('');
+    };
+    extract($array_data);
+    ob_start();
+    require_once ($address_template);
+
+    return ob_get_clean();
 
 }
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 require_once('functions.php');
-require_once('templates/layout.php');
-require_once('templates/index.php');
+require_once ('templates/data_templates.php');
 
+$page_content = render_template('templates/index.php', ['ads_list' => $ads_list ]);
+$layout_content = render_template("templates/layout.php", [
+
+        ]);
+print ($layout_content);
