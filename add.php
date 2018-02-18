2196633a811cf77d29702d52d186c7d961d3b952
kovarnyirus <kovarnyi@gmail.com>
2018-02-18 18:02:04 +0300

    вынес проверку и перенос изображения в одельную функцию

--- add.php
+++ add.php
@@ -28,21 +28,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
 
-    if ($_FILES ['lot_img']['name']) {
-        $tmp_name = $_FILES['lot_img']['tmp_name'];
-        $path = $_FILES['lot_img']['name'];
-        $info = finfo_open(FILEINFO_MIME_TYPE);
-        $file_type = finfo_file($info, $tmp_name);
+    function check_img($file, $file_format, $move_path) {
+        if ($file['name']) {
+            $tmp_name = $file['tmp_name'];
+            $path = $file['name'];
+            $info = finfo_open(FILEINFO_MIME_TYPE);
+            $file_type = finfo_file($info, $tmp_name);
 
-        if ($file_type == "image/jpeg" || $file_type == "image/png") {
-            move_uploaded_file($tmp_name, 'img/' . $path);
-            $lot['lot_img'] = 'img/' . $path;
+            foreach ($file_format as $value)
+            if ($file_type == $value) {
+                move_uploaded_file($tmp_name, $move_path . $path);
+                return $img_path = [ 'img_path' => $move_path . $path];
+            } else {
+                return 'format error';
+            }
         } else {
-            $errors['lot_img'] = 'Загрузите картинку в формате JPG или PNG';
+           return 'no file';
         }
-    } else {
-        $errors['lot_img'] = 'Вы не загрузили файл';
     };
+
+    switch ($img = check_img($_FILES ['lot_img'], ['image/jpeg', 'image/png'],'img/')) {
+        case 'format error':
+            $errors['lot_img'] = 'Загрузите картинку в формате JPG или PNG';
+            break;
+        case 'no file':
+            $errors['lot_img'] = 'Вы не загрузили файл';
+            break;
+        default:
+            $lot['lot_img'] = $img['img_path'];
+    }
 
     if (count($errors)) {
         $page_content = render_template('add',
