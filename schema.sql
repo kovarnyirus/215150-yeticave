c54fd9591745f0153ed3177bd4d77f64daf437b7
kovarnyirus <kovarnyi@gmail.com>
2018-02-22 00:49:58 +0300

    создание таблиц

---
+++ schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE yeticave
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+USE yeticave;
+
+CREATE TABLE category (
+  id       INT AUTO_INCREMENT PRIMARY KEY,
+  category CHAR(50)
+);
+
+CREATE TABLE lot (
+  id             INT AUTO_INCREMENT PRIMARY KEY,
+  lot_create     DATETIME,
+  name           CHAR(70) NOT NULL ,
+  description    TEXT,
+  lot_img        CHAR(100),
+  initial_price  INT NOT NULL ,
+  completio_date DATE NOT NULL ,
+  step           INT NOT NULL
+);
+
+CREATE TABLE bet (
+  id         INT AUTO_INCREMENT PRIMARY KEY,
+  bet_date   DATE,
+  user_price INT
+);
+
+CREATE TABLE user (
+  id                INT AUTO_INCREMENT PRIMARY KEY,
+  data_registration DATETIME,
+  email             CHAR(30) NOT NULL ,
+  name              CHAR(30) NOT NULL ,
+  password          CHAR(68) NOT NULL ,
+  avatar            CHAR(60),
+  contacts          CHAR(50)
+);
