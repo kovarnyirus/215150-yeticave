3ca83aaf857ac785b21010252487bce4bc0f1402
kovarnyirus <kovarnyi@gmail.com>
2018-02-22 13:07:43 +0300

    обязательное поле

--- schema.sql
+++ schema.sql
@@ -11,26 +11,26 @@ CREATE TABLE category (
 CREATE TABLE lot (
   id             INT AUTO_INCREMENT PRIMARY KEY,
   lot_create     DATETIME,
-  name           CHAR(70) NOT NULL ,
-  description    TEXT,
-  lot_img        CHAR(100),
-  initial_price  INT NOT NULL ,
-  completio_date DATE NOT NULL ,
-  step           INT NOT NULL
+  name           CHAR(70)  NOT NULL,
+  description    TEXT      NULL,
+  lot_img        CHAR(100) NULL,
+  initial_price  INT       NOT NULL,
+  completio_date DATE      NOT NULL,
+  step           INT       NOT NULL
 );
 
 CREATE TABLE bet (
   id         INT AUTO_INCREMENT PRIMARY KEY,
-  bet_date   DATE,
-  user_price INT
+  bet_date   DATE NOT NULL,
+  user_price INT  NOT NULL
 );
 
 CREATE TABLE user (
   id                INT AUTO_INCREMENT PRIMARY KEY,
   data_registration DATETIME,
-  email             CHAR(30) NOT NULL ,
-  name              CHAR(30) NOT NULL ,
-  password          CHAR(68) NOT NULL ,
+  email             CHAR(30) NOT NULL UNIQUE,
+  name              CHAR(30) NOT NULL,
+  password          CHAR(68) NOT NULL UNIQUE,
   avatar            CHAR(60),
   contacts          CHAR(50)
 );
